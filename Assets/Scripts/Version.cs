291cdd9c654ada2c48160d5e7d94e278cc8517b7
Yu Tanaka <YRK00337@nifty.com>
2019-09-03 16:28:44 +0900

    ver0.8 webglビルド

---
+++ Assets/Scripts/Version.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Version : MonoBehaviour
+{
+    void Start()
+    {
+        TextMeshProUGUI vt = GetComponent<TextMeshProUGUI>();
+        vt.text = $"Ver {Application.version}";
+    }
+}
