cf4d9d36f0021acee3accbe939cdec85f68f70b3
Yu Tanaka <YRK00337@nifty.com>
2019-09-03 21:59:23 +0900

    ランキング実装

--- Assets/Scripts/TitleBehaviour.cs
+++ Assets/Scripts/TitleBehaviour.cs
@@ -1,26 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleBehaviour : MonoBehaviour
 {
     bool isStarted = false;
+    bool isRanking = false;
 
     private void Start()
     {
         SoundManager.PlayBGM(SoundManager.AUDIO_LIST.TITLE);
+        if (GameParams.isHighScore)
+        {
+            ShowRanking();
+        }
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        if (isStarted) return;
-
-        if (Input.GetMouseButtonDown(0))
+        if (isRanking)
         {
-            isStarted = true;
-            UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game");
-            SoundManager.StopBGM();
-            SoundManager.PlaySE(SoundManager.AUDIO_LIST.START);            
+            if (!SceneManager.GetSceneByName("Ranking").IsValid())
+            {
+                isRanking = false;
+            }
         }
+    }
+
+    public void GameStart()
+    {
+        if (isStarted || isRanking) return;
+
+        isStarted = true;
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("Game");
+        SoundManager.StopBGM();
+        SoundManager.PlaySE(SoundManager.AUDIO_LIST.START);
+    }
+
+    public void ShowRanking()
+    {
+        if (isRanking) return;
+
+        isRanking = true;
+        naichilab.RankingLoader.Instance.SendScoreAndShowRanking(GameParams.highScore);
     }
 }
